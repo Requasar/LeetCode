1cb9e19bd39e04a1a861d27f481d2949172fb2ec
Alperen Enes Efe <56564362+Requasar@users.noreply.github.com>
2024-07-15 15:09:52 +0300

    Update plus_one.cpp

--- plus_one.cpp
+++ plus_one.cpp
@@ -17,3 +17,31 @@ public:
     }
 
 };
+
+
+/*
+        int size = digits.size();
+        
+        // Convert vector to integer
+        int number = 0;
+        for (int i = 0; i < size; i++) {
+            number = number * 10 + digits[i];
+        }
+        
+        // Increment the integer by 1
+        number++;
+        
+        // Convert integer back to vector of digits
+        std::vector<int> result;
+        while (number > 0) {
+            int digit = number % 10;
+            result.push_back(digit);
+            number /= 10;
+        }
+        
+        // Reverse the vector to get the correct order
+        std::reverse(result.begin(), result.end());
+        
+        return result;
+    }
+*/
