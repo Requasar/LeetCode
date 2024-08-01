5d7670ef04adce702fe109496a80f0516240955d
Alperen Enes Efe <56564362+Requasar@users.noreply.github.com>
2024-08-01 15:05:41 +0300

    Create Single_Number.cpp

---
+++ Single_Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        size_t size = nums.size();
+        if(size == 1)
+            return nums[0];
+        sort(nums.begin(),nums.end());
+
+        for(int i=1 ; i < size ; i+=2)
+        {
+            if(nums[i] != nums[i-1])
+                return nums[i-1];
+        }
+        return nums[size-1];
+        
+    }
+};
