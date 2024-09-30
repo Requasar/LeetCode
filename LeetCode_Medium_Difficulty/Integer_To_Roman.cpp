13f18c5172f6a2a064eb00f84d43fe65cd43a4b9
Alperen Enes Efe <56564362+Requasar@users.noreply.github.com>
2024-09-30 19:55:52 +0300

    Update Integer_To_Roman.cpp

--- LeetCode_Medium_Difficulty/Integer_To_Roman.cpp
+++ LeetCode_Medium_Difficulty/Integer_To_Roman.cpp
@@ -8,5 +8,20 @@ public:
         string ths[]={"","M","MM","MMM"};
         
         return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        /*
+        int[] n = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] s = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int i =0;
+        String str = new String(); 
+        while (num>0){
+            if (num>=n[i]){
+                str=str+s[i];
+                num-=n[i];
+            } else{
+                i++;
+            }
+        }
+        return str;
+        */
     }
 };
